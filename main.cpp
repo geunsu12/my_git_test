17c50d92331cc5d55a3d7fe38b17c0e3576ddbbe
my_git_test <geunsu23@naver.com>
2020-04-16 18:58:36 +0900

    done

--- main.cpp
+++ main.cpp
@@ -2,7 +2,22 @@
 
 int sorted[10];
 
+
 int main(){
+    int i;
+    int a;
+    int srt[10];
+    
+    for(i=0;i<10;i++){
+        cin >> a;
+        srt[i] = a;
+    }
+
+    merge_sort(srt, 0, 9);
+
+    for(i=0;i<10;i++){
+        cout << sorted[i] <<" ";
+    }
 
     return 0;
 }
